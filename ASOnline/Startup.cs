817be8710e91a960904f6fb8746fac54186f4522
shkumatartem <shkumatartem@gmail.com>
2017-01-17 22:32:50 +1300

    Initial

---
+++ ASOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASOnline.Startup))]
+namespace ASOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
